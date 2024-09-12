88a5509b28931352a67a26ea564b405e1f10f72e
td2004 <tdarpitha2004@gmail.com>
2024-09-12 14:50:55 +1000

    changes

---
+++ index.js
@@ -0,0 +1,16 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import { registerRootComponent } from 'expo'; // If you're using Expo
+
+// For web:
+import { createRoot } from 'react-dom/client';
+import { Platform } from 'react-native';
+
+if (Platform.OS === 'web') {
+  const container = document.getElementById('app');
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  AppRegistry.registerComponent(appName, () => App);
+}
